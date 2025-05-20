30f858730cfdacffc4d7e7ed663218459e700b43
xnd0xgx1 <42785791+xnd0xgx1@users.noreply.github.com>
2025-05-20 11:58:22 -0600

    prompt JSON

--- src/repository/aoi_repository.py
+++ src/repository/aoi_repository.py
@@ -23,73 +23,77 @@ class AOIRepository(AOIInterface):
             messages=[
                 {
                     "role": "system",
-                    "content": """Eres un agente especializado en extracción de campos de contratos, debes extraer siempre los siguientes campos en un json, RETORNA SOLO JSON siempre en un formato válido sin añadir o modificar los nombres de los campos, Este es un ejemplo de formato (NO lo uses como contenido de respuesta),SIEMPRE DEBES TRAER AL MENOS UNA COBERTURA: 
-                        [{'ContratoOrden': 4620004842,
-                        'ContratoMarco': '4620004841',
-                        'NitProveedor': '901212206-9',
-                        'NombreProveedor': 'DATA KNOW S.A.S',
-                        'Objeto': 'Prestación de servicios de soporte, gestión, administración, monitoreo, mantenimiento y desarrollo de RPAs y analítica avanzada',
-                        'GestionGarantiasDoc': True,
-                        'Cobertura': 'Garantía de Cumplimiento',
-                        'DescripcionCobertura': 'Garantía que ampara las obligaciones del Contratista para cada orden de entrega, incluyendo los proyectos que superan 200 millones de pesos',
-                        'CoberturaPara': 'Contrato',
-                        'PorcentajeCobertura': 10,
-                        'TextoTiempoAdicionalCobertura': 'Vigente desde la firma de cada orden y hasta un (1) mes después de su finalización',
-                        'TiempoAdicionalCobertura': 1,
-                        'DescripcionValorDoc': 'El valor del contrato es indeterminado, pero determinable al vencimiento, basado en la sumatoria de órdenes de servicios liquidadas',
-                        'ValorDoc': 'INDETERMINADO',
-                        'Moneda': 'COP',
-                        'PlazoVigenciaDoc': '5 años desde la suscripción del contrato',
-                        'PlazoDoc': 60,
-                        'FechaInicioCobertura': '01/01/2025',
-                        'FechaFinCobertura': '30/01/2025',
-                        'OrdenInicio': 1}]
+                    "content": """Eres un agente especializado en extracción de campos de contratos, debes extraer siempre los siguientes campos en un JSON, RETORNA SOLO JSON siempre en un formato válido sin añadir o modificar los nombres de los campos. Adicionalmente, no utilice comillas sencillas y tampoco coloque comas para separar los valores numéricos.
 
-                                debe ser un Objeto del array por cada cobertura, verifica bien el texto para identificar que si existan varias coberturas, SIEMPRE EXISTE AL MENOS 1, 
-                                Descripción de campos para la extracción de datos de documentos contractuales:
-                                
-                                ContratoOrden: Número de 10 dígitos que se encuentra en el título del documento. En adjudicaciones, aparece después de "El número del Contrato es" y en órdenes de entrega, luego de "ORDEN DE ENTREGA No".
+Este es un ejemplo de formato (NO lo uses como contenido de respuesta). SIEMPRE DEBES TRAER AL MENOS UNA COBERTURA:
+[
+{
+"ContratoOrden": 4620004842,
+"ContratoMarco": "4620004841",
+"NitProveedor": "901212206-9",
+"NombreProveedor": "DATA KNOW S.A.S",
+"Objeto": "Prestación de servicios de soporte, gestión, administración, monitoreo, mantenimiento y desarrollo de RPAs y analítica avanzada",
+"GestionGarantiasDoc": true,
+"Cobertura": "Garantía de Cumplimiento",
+"DescripcionCobertura": "Garantía que ampara las obligaciones del Contratista para cada orden de entrega, incluyendo los proyectos que superan 200 millones de pesos",
+"CoberturaPara": "Contrato",
+"PorcentajeCobertura": 10,
+"TextoTiempoAdicionalCobertura": "Vigente desde la firma de cada orden y hasta un (1) mes después de su finalización",
+"TiempoAdicionalCobertura": 1,
+"DescripcionValorDoc": "El valor del contrato es indeterminado, pero determinable al vencimiento, basado en la sumatoria de órdenes de servicios liquidadas",
+"ValorDoc": "INDETERMINADO",
+"Moneda": "COP",
+"PlazoVigenciaDoc": "5 años desde la suscripción del contrato",
+"PlazoDoc": 60,
+"FechaInicioCobertura": "01/01/2025",
+"FechaFinCobertura": "30/01/2025",
+"OrdenInicio": 1
+}
+]
 
-                                ContratoMarco: Campo opcional. En algunos casos, se puede encontrar después de "No CONTRATO" en órdenes de entrega.
+Debe ser un objeto del array por cada cobertura, verifica bien el texto para identificar que si existen varias coberturas; SIEMPRE EXISTE AL MENOS 1.
 
-                                NitProveedor: Se debe extraer de la lista contratopolizagarantia usando el número de Contrato/Orden como llave. Si no se encuentra, se deja vacío.
+Descripción de campos para la extracción de datos de documentos contractuales:
 
-                                NombreProveedor: Igual que el NIT, se extrae desde la lista contratopolizagarantia, usando como clave el Contrato/Orden.
+ContratoOrden: Número de 10 dígitos que se encuentra en el título del documento. En adjudicaciones, aparece después de "El número del Contrato es" y en órdenes de entrega, luego de "ORDEN DE ENTREGA No".
 
-                                Objeto: También se obtiene desde contratopolizagarantia, a partir del Contrato/Orden.
+ContratoMarco: Campo opcional. En algunos casos, se puede encontrar después de "No CONTRATO" en órdenes de entrega.
 
-                                GestionGarantiasDoc: Si en el documento se encuentra el título “GARANTÍAS, FIANZAS Y SEGUROS” y hay contenido debajo, se marca como “SI”; de lo contrario, se coloca “NO”.
+NitProveedor: Se debe extraer de la lista contratopolizagarantia usando el número de Contrato/Orden como llave. Si no se encuentra, se deja vacío.
 
-                                Coberturas: Se refiere a cada subtítulo listado después del título “GARANTÍAS, FIANZAS Y SEGUROS” o “GARANTÍAS Y SEGUROS”, PUEDE VARIAR LOS SUBTITULOS EMPIEZAN CON a) b), etc...
+NombreProveedor: Igual que el NIT, se extrae desde la lista contratopolizagarantia, usando como clave el Contrato/Orden.
 
-                                DescripcionCobertura: Texto que detalla la cobertura, incluyendo fecha, porcentaje y beneficiario. En documentos marco, también se extrae desde la lista correspondiente.
+Objeto: También se obtiene desde contratopolizagarantia, a partir del Contrato/Orden.
 
-                                CoberturaPara: Indica si la cobertura aplica para el Contrato o para las Órdenes. Se debe traducir y dejar claro.
+GestionGarantiasDoc: Si en el documento se encuentra el título “GARANTÍAS, FIANZAS Y SEGUROS” y hay contenido debajo, se marca como true; de lo contrario, false.
 
-                                PorcentajeCobertura: Se extrae desde la Descripción Cobertura y se convierte a valor numérico.
+Coberturas: Se refiere a cada subtítulo listado después del título “GARANTÍAS, FIANZAS Y SEGUROS” o “GARANTÍAS Y SEGUROS”. PUEDE VARIAR, los subtítulos empiezan con a), b), etc.
 
-                                TextoTiempoAdicionalCobertura: Fragmento textual donde se especifique plazo adicional de cobertura. Si no se encuentra, se deja vacío.
+DescripcionCobertura: Texto que detalla la cobertura, incluyendo fecha, porcentaje y beneficiario. En documentos marco, también se extrae desde la lista correspondiente.
 
-                                TiempoAdicionalCobertura: Se traduce y extrae como está en el documento (ej. "6 meses adicionales").
+CoberturaPara: Indica si la cobertura aplica para el Contrato o para las Órdenes. Se debe traducir y dejar claro.
 
-                                DescripcionValorDoc: Texto ubicado después de frases como “El valor del contrato es” o “VALOR ANTES DE IMPUESTOS”.
+PorcentajeCobertura: Se extrae desde la DescripciónCobertura y se convierte a valor numérico.
 
-                                ValorDoc: Se extrae del documento. Si el valor está en letras, se debe traducir a número. Si no es posible determinarlo, se marca como “INDETERMINADO”.
+TextoTiempoAdicionalCobertura: Fragmento textual donde se especifique plazo adicional de cobertura. Si no se encuentra, se deja vacío.
 
-                                Moneda: Se obtiene a partir del texto asociado a la moneda (COP, USD, EUR), y puede estar antes o después del valor.
+TiempoAdicionalCobertura: Se extrae como número de unidades (por ejemplo, 6).
 
-                                PlazoVigenciaDoc: Texto encontrado después de expresiones como “VIGENCIA Y PLAZO DEL CONTRATO” o “PLAZO DE LA ORDEN DE ENTREGA”. Especifica duración y vigencia del contrato u orden.
+DescripcionValorDoc: Texto ubicado después de frases como “El valor del contrato es” o “VALOR ANTES DE IMPUESTOS”.
 
-                                PlazoDoc: De este campo se extrae el plazo en días, meses o años, y la fecha que se menciona para la constitución de garantías. Si no se encuentra, se deja vacío.
+ValorDoc: Se extrae del documento. Si el valor está en letras, se debe traducir a número. Si no es posible determinarlo, se marca como "INDETERMINADO".
 
-                                FechaInicioCobertura: En contratos, se refiere a la fecha ubicada en secciones como “Resumen de eventos / Completado”. Para órdenes, también puede aparecer explícitamente (devuelve el valor en formato de fecha).
+Moneda: Se obtiene a partir del texto asociado a la moneda (COP, USD, EUR).
 
-                                FechaFinCobertura: Si la cobertura es para el contrato, se calcula sumando el Plazo Doc a la Fecha Inicio Cobertura. Si no aplica, se deja vacío  (devuelve el valor en formato de fecha).
+PlazoVigenciaDoc: Texto encontrado después de expresiones como “VIGENCIA Y PLAZO DEL CONTRATO” o “PLAZO DE LA ORDEN DE ENTREGA”.
 
-                                OrdenInicio: Se marca como “SI” si en el documento se menciona que hay una orden de inicio (por ejemplo: “ORDEN DE ENTREGA / ORDEN DE SERVICIOS”); en caso contrario, “NO”.
+PlazoDoc: Se extrae el plazo en días. Si aparece en meses o años, convertir a días. Si no se encuentra, se deja vacío.
 
+FechaInicioCobertura: Devuelve el valor en formato dd/MM/yyyy.
 
+FechaFinCobertura: Si la cobertura es para el contrato, se calcula sumando el PlazoDoc a la FechaInicioCobertura. Si no aplica, se deja vacío.
 
+OrdenInicio: Se marca como 1 si existe orden de inicio; de lo contrario, 0.
                     """,
                 },
                 {
